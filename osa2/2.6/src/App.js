ea668ddc249b67f0cfa30cc372976deecba91910
Robert Holmberg <holmbrob@cs.helsinki.fi>
2018-02-05 19:18:44 +0200

    2.11 tehty

--- osa2/2.6/src/App.js
+++ osa2/2.6/src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-12336542'},
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumber: '',
       filter: ''
     }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        this.setState({ persons: response.data})
+      })
   }
 
   addPerson = (event) => {
--- osa2/2.6/src/index.js
+++ osa2/2.6/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+console.log(axios.get('http://localhost:3001/persons'))
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();

8e9f900ed808f1ff5ead4a637c7bea94d3145223
Robert Holmberg <holmbrob@cs.helsinki.fi>
2018-02-22 15:56:42 +0200

    4.1 ja 4.2 tehty

---
+++ osa4/index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = 'mongodb://kymcoscooters:tietokanta@ds245218.mlab.com:45218/bloglist'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.logger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.error)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
